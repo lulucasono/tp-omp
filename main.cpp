9307adafde822ff771c6eaf9396f3e107139f96e
Pierre-Louis Lefebvre <pierre-louis.lefebvre@insa-lyon.fr>
2018-11-12 15:16:46 +0100

    sum

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
+#include "main.h"
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
 
 #define NB_MAX 100
 
-int main()
+int main(int argc, char **argv)
 {
     int vector1[NB_MAX];
     int vector2[NB_MAX];
@@ -15,8 +17,35 @@ int main()
     for (i = 0; i < NB_MAX; i++)
     {
         vector1[i] = rand();
-        std::cout << vector1[i] << std::endl;
         vector2[i] = rand();
-        std::cout << vector2[i] << std::endl;
     }
+
+    display(vector1, NB_MAX);
+    display(vector2, NB_MAX);
+
+    std::cout << sum(vector1, NB_MAX) << std::endl;
+
+    return 0;
+}
+
+void display(int *vec, int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+        std::cout << vec[i] << std::endl;
+
+    std::cout << std::endl;
+}
+
+long sum(int *vec, int length)
+{
+    int i;
+    long sum = 0;
+    
+    for (i = 0; i < length; i++)
+    {
+        sum += vec[i];
+    }
+
+    return sum;
 }
