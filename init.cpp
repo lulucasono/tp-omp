0c0ffb4298db6236ee8f57da1ad3e8d2ab54fb37
lucas ono <lulucasono@gmail.com>
2018-11-12 14:50:31 +0100

    first sequential iteration

--- init.cpp
+++ init.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include <math>
+#include <chrono>
 #include <vector>
 #define NB_MAX 100
-using namespace std::chrono;
-void main (){
+int main (){
+    using namespace std::chrono;
     //NB_MAX will be used as an arg
     std::vector<int> vec (NB_MAX, 0);
-    high_resolution_clock::time_point start = high_resolution_clock
+    high_resolution_clock::time_point start = high_resolution_clock::now();
     for(int i = 0; i < NB_MAX; i++){
-    	vec[i] = math.random();
+    	vec[i] = rand();
     }
+    high_resolution_clock::time_point end = high_resolution_clock::now();
+    duration<long> time_duration = duration_cast<duration<long>>(end -start);
+    std::cout<< "Duration NB_MAX"<<std::endl<<time_duration.count()<<NB_MAX<<std::endl;
 }
