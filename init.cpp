cdcc5a640a205718a460671447376a622453927c
Lucas Ono <lono@flyon>
2018-11-12 14:29:59 +0100

    first commit

---
+++ init.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math>
+#include <vector>
+#define NB_MAX 100
+using namespace std::chrono;
+void main (){
+    //NB_MAX will be used as an arg
+    std::vector<int> vec (NB_MAX, 0);
+    high_resolution_clock::time_point start = high_resolution_clock
+    for(int i = 0; i < NB_MAX; i++){
+    	vec[i] = math.random();
+    }
+}
