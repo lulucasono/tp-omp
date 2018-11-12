95f29929b99398779857252bfcd3baf3fe380c86
Pierre-Louis Lefebvre <pierre-louis.lefebvre@insa-lyon.fr>
2018-11-12 15:30:06 +0100

    merge main

--- main.cpp
+++ main.cpp
@@ -1,16 +1,31 @@
 #include "main.h"
 
 #include <iostream>
+#include <chrono>
 #include <ctime>
 #include <cstdlib>
 
 #define NB_MAX 100
 
+using namespace std::chrono;
+
 int main(int argc, char **argv)
 {
+    int i;
+    
+    int nbMax = NB_MAX;
+    if(argc < 2)
+    {
+    	// nothing
+    }
+    else
+    {
+    	nbMax = std::stoi(argv[1]);
+    }
+
+    // NB_MAX will be used as an arg
     int vector1[NB_MAX];
     int vector2[NB_MAX];
-    int i;
 
     srand(time(NULL));
 
@@ -22,9 +37,13 @@ int main(int argc, char **argv)
 
     display(vector1, NB_MAX);
     display(vector2, NB_MAX);
-
-    std::cout << sum(vector1, NB_MAX) << std::endl;
-
+    
+    high_resolution_clock::time_point start = high_resolution_clock::now();
+    long sumResult = sum(vector1, NB_MAX);
+    high_resolution_clock::time_point end = high_resolution_clock::now();
+    duration<long> time_duration = duration_cast<duration<long>>(end -start);
+    std::cout << "Duration NB_MAX" << std::endl << time_duration.count() << " " << nbMax << std::endl;
+    std::cout << "Vector 1 sum : " << sumResult << std::endl;
     return 0;
 }
 
